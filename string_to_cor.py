05547c2902249621bbc444ca2c70c77f7f6e28ca
fu <scotfu@gmail.com>
2014-04-19 11:26:12 -0400

    update

--- string_to_cor.py
+++
@@ -1,9 +0,0 @@
-# !/usr/bin/env python
-
-data = open('coordinates.1000')
-out = open('coor.csv','w')
-for line in data:
-    out.write(str(','.join([c for c in line if c!='\n'])+'\n'))
-    
-data.close()
-out.close()
