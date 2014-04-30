a87015a9569b1346aec874e920870ee9fb13e335
fu <scotfu@gmail.com>
2014-04-30 09:32:41 -0400

    new urls for the last feature

--- web_api/records/urls.py
+++ web_api/records/urls.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 #coding=utf-8
 from django.conf.urls import patterns, url
-from .views import record, bitstring, record_search, bit_string_search,record_search_aggregation,test
+from .views import record, bitstring, record_search, bit_string_search,record_search_aggregation,record_search_group,test
 
 
 urlpatterns = patterns('',
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     url(r'^bitstring/(?P<b_id>\d+)/$', bitstring,name='bit_string'),
     url(r'^record/search/$', record_search,name='record_search'),
     url(r'^record/search/aggregation/$', record_search_aggregation,name='record_search_aggregation'),
+    url(r'^record/search/group/$', record_search_group,name='record_search_group'),
     url(r'^bitstring/search/$', bit_string_search,name='bit_string_search'),
     url(r'^test/$',test,name='test'),
     )
